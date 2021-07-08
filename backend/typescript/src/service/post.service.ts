2f490ab9c1d0bde581fbdbe7609d248cf2524755
Wasswa Richard <wasswa@awamo.com>
2021-07-08 12:08:06 +0300

    project setup

--- backend/typescript/src/service/post.service.ts
+++ backend/typescript/src/service/post.service.ts
@@ -5,18 +5,11 @@ export function createPost(input: DocumentDefinition<PostDocument>) {
     return Post.create(input);
 }
 
-export function findPost(
-    query: FilterQuery<PostDocument>,
-    options: QueryOptions = { lean: true }
-) {
+export function findPost(query: FilterQuery<PostDocument>, options: QueryOptions = { lean: true }) {
     return Post.findOne(query, {}, options);
 }
 
-export function findAndUpdate(
-    query: FilterQuery<PostDocument>,
-    update: UpdateQuery<PostDocument>,
-    options: QueryOptions
-) {
+export function findAndUpdate(query: FilterQuery<PostDocument>, update: UpdateQuery<PostDocument>, options: QueryOptions) {
     return Post.findOneAndUpdate(query, update, options);
 }
 
